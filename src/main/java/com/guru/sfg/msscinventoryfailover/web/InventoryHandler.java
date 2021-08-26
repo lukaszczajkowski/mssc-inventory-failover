290c34f8038172a21b6472f895636aff53963e79
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-26 10:31:11 +0200

    Dockerfile configuration

--- src/main/java/com/guru/sfg/msscinventoryfailover/web/InventoryHandler.java
+++ src/main/java/com/guru/sfg/msscinventoryfailover/web/InventoryHandler.java
@@ -17,7 +17,7 @@ public class InventoryHandler {
 
     public Mono<ServerResponse> listInventory(ServerRequest request) {
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_NDJSON)
+                .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(Arrays.asList(BeerInventoryDto.builder()
                         .id(UUID.randomUUID())
                         .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
